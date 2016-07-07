f1606e5469099ccd8284276230e48877c9ffabd2
Álvaro <atrigol@gmail.com>
2016-07-07 16:57:33 +0200

    - Added gulp and `dist` files - Updated package.json - Improved credits comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,34 @@
-/*-------------------------------------------------------------------
-  Required plugins
--------------------------------------------------------------------*/
 var gulp = require('gulp');
 var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-clean-css');
 
-gulp.task('js', function() {
-  gulp.src('./jquery.multiscroll.js')
-    .pipe(uglify({
-        preserveComments: 'license'
-      }))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./dist'));
+gulp.task('css', function() {
+    gulp.src('./jquery.multiscroll.css')
+        .pipe(sourcemaps.init())
+        .pipe(gulp.dest('./dist'))
+        .pipe(minifyCss({
+            compatibility: 'ie8',
+            advanced: false,
+            keepSpecialComments: '1'
+        }))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('.')) 
+        .pipe(gulp.dest('./dist'));
 });
 
-// Watcher
-gulp.task('default', ['js']);
+gulp.task('js', function() {
+    gulp.src('./jquery.multiscroll.js')
+        .pipe(sourcemaps.init())
+        .pipe(gulp.dest('./dist'))
+        .pipe(uglify({
+            preserveComments: 'license'
+        }))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['css', 'js']);
